6ce9cfa195c7c78dc063b6b94ceec04ac2776559
tomnatt <github@tomnatt.com>
2016-02-23 13:40:34 +0000

    Babel build and minified output, introducted Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    babel: {
+      options: {
+        presets: ['es2015', 'react']
+      },
+      dist: {
+        files: {
+          'js/output.js': ['src/form.js']
+          }
+      }
+    },
+
+    uglify: {
+      options: {
+      },
+      dist: {
+        files: {
+          'js/react.js': ['src/react.js', 'src/react-dom.js'],
+          'js/app.js': ['js/output.js']
+        }
+      }
+    }
+
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['babel', 'uglify']);
+
+};
